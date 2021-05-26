75302932935e9ca7f28009ec8cf34433d6dc5a31
Matej Čurilla <curilla.m@gmail.com>
2021-05-26 16:07:01 +0200

    import cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,26 @@ export { AmenitySource } from './amenity_source';
 export { UserLocationSource } from './user_location_source';
 export { GeoJSONSource } from './geojson_source'
 export { RoutingSource } from './routing_source'
-export { AmenityCategory, Amenity, ProximiioMapboxSyncStatus, ProximiioMapboxEvents } from './instance'
-export { FeatureCollection } from './types'
+export {
+  Amenity,
+  AmenityCategory,
+  ProximiioMapbox,
+  ProximiioMapboxSyncStatus,
+  ProximiioMapboxEvents
+} from './instance'
+export {
+  FeatureCollection,
+  ProximiioMapboxRoute,
+  ProximiioUnitConversion,
+  ProximiioFeatureType,
+  ProximiioRouteEvent,
+  ProximiioWayfindingOptions,
+  ProximiioRouteConfiguration,
+  ProximiioRouteUpdateType,
+  RouteStepDescriptor,
+  RouteStepSymbol
+} from './types'
 export { Feature } from './feature'
-export { ProximiioMapbox } from './instance'
 import instance from './instance'
 export default instance;
 
